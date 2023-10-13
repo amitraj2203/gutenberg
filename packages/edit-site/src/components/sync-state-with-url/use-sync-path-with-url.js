e111212831f889d1274a0b663ce75ae0b0fec158
Ella <ella@vandurpe.com>
2023-10-13 12:27:37 +0300

    Restore useSyncPathWithURL

--- packages/edit-site/src/components/sync-state-with-url/use-sync-path-with-url.js
+++ packages/edit-site/src/components/sync-state-with-url/use-sync-path-with-url.js
@@ -90,6 +90,7 @@ export default function useSyncPathWithURL() {
 				updateUrlParams( {
 					postType: 'page',
 					postId: navigatorParams?.postId,
+					path: undefined,
 				} );
 			} else if ( navigatorLocation.path === '/patterns' ) {
 				updateUrlParams( {
